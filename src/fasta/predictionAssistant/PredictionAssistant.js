36b0f63612fe32ce05cad844f1fa94939cdfe30b
FanPu1 <fanpu.cs@gmail.com>
2020-07-18 23:20:22 -0500

    BH-019: implement setPatterns method in PredictionAssistant class

--- src/fasta/predictionAssistant/PredictionAssistant.js
+++ src/fasta/predictionAssistant/PredictionAssistant.js
@@ -1,3 +1,5 @@
+const convertPatternToRegExp = require("../../util/patternConverter");
+
 /**
  * A class contains all methods related with the output of sequence prediction.
  */
@@ -9,9 +11,160 @@ class PredictionAssistant {
    */
   constructor(fastaSequenceObject) {
     this.fastaSequenceObject = fastaSequenceObject;
+    this.patternsWithIds = null;
   }
 
+  /**
+   * Sets the pattern used for prediction. Pattern (motif) is string which is used to check if sequence (string) is matched. 
+   * User should use the motif accroding to https://prosite.expasy.org/scanprosite/scanprosite_doc.html#mo_motifs
+   * 
+   * Pattern syntax
+   * The standard IUPAC one letter code for the amino acids and nucleotide code.
+   * - The symbol 'x' is used for a position where any amino acid is accepted.
+   * - Ambiguities are indicated by listing the acceptable amino acids for a given position, between square brackets '[ ]'. For example: [ALT] stands for Ala or Leu or Thr.
+   * - Ambiguities are also indicated by listing between a pair of curly brackets '{ }' the amino acids that are not accepted at a given position. For example: {AM} stands for all any amino acid except Ala and Met.
+   * - Each element in a pattern is separated from its neighbor by a '-'.
+   * - Repetition of an element of the pattern can be indicated by following that element with a numerical value or, if it is a gap ('x'), by a numerical range between parentheses.
+   * - Examples:
+   *  - x(3) corresponds to x-x-x
+   *  - x(2,4) corresponds to x-x or x-x-x or x-x-x-x
+   *  - A(3) corresponds to A-A-A
+   * - When a pattern is restricted to either the N- or C-terminal of a sequence, that pattern respectively starts with a '<' symbol or ends with a '>' symbol.
+   * - In some rare cases (e.g. PS00267 or PS00539), '>' can also occur inside square brackets for the C-terminal element. 'F-[GSTV]-P-R-L-[G>]' is equivalent to 'F-[GSTV]-P-R-L-G' or 'F-[GSTV]-P-R-L>'.
+   * 
+   * User can have the following input:
+   * 1. A pattern string without pattern Id. Example 1: "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]"
+   * 2. An array of strings, and each element in this array is a pattern used to prediction. Element in this array cannot be repeat. Example 2:
+   *        ["[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]", "[DNS]-x-[DNS]-{FLIVWY}"]
+   * 3. A JSON object containing a pattern and a pattern Id. Example 3: 
+   *        {"patternId_1": "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]"}
+   *        OR
+   *        {
+   *          "patternId_1": {
+   *            "pattern": "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]",
+   *            "description": "This is a desciption of this pattern, what is used for, where it is come from",
+   *            "url":"www.reference_url.io"
+   *          }, 
+   *          "patternId_2": "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]"
+   *        }
+   * @param  {String}, {Arrary}, {Object} in above desciption
+   * @returns {PredictionAssistant} object with patternMap being set.
+   * @throws a exception if input is null, undefined, a blank string, or an empty string.
+   */
+  setPatterns(patterns){
+    // check invalid input
+    if (patterns === null || patterns === undefined) {
+      throw new Error("The input patterns cannot be null or undefined.");
+    }
+    // convert input into a {PatternMap} object
+    switch(typeof patterns){
+    case "string":  // like example 1
+      if(patterns.trim() === "") {
+        throw new Error("The input patterns cannot be blank string or an empty string.");
+      }
+      this.patternsWithIds = this.translatePatternString(patterns);
+      break;
+    case "object":
+      if(Array.isArray(patterns)) { // if input is an array with string element, like example 2
+        this.patternsWithIds = this.translatePatternArray(patterns);
+      } else { // treat as example 3.
+        this.patternsWithIds = this.translatePatternObject(patterns);
+      }
+      break;
+    default:
+      throw new Error("The format of input patterns is not supported.");
+    } 
+    return this;
+  }
 
+  /**
+   * Translates a string to a {PatternMap}.
+   * input example: "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]"
+   * @param {Sting} stringPattern 
+   * @returns a {PatternMap}.
+   */
+  translatePatternString(stringPattern) {
+    const patternId = stringPattern; // patternId is same as input string
+    const patternInfo= {
+      "pattern": convertPatternToRegExp(stringPattern),
+      "description": null,
+      "url": null
+    };
+    const outputPatternMap = new Map();
+    outputPatternMap.set(patternId, patternInfo);
+
+    return outputPatternMap;
+  }
+
+  /**
+   * Translates a JSON object containing keys and values into a {PatternMap}.
+   * Input patern object example -> A JSON object containing a pattern and a pattern Id:
+   *        {"patternId_1": "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]"}
+   *        OR
+   *        {
+   *          "patternId_1": {
+   *            "pattern": "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]",
+   *            "description": "This is a desciption of this pattern, what is used for, where it is come from",
+   *            "url":"www.reference_url.io"
+   *          }, 
+   *          "patternId_2": "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]"
+   *        }
+   * @param {Object} patternObject 
+   */
+  translatePatternObject(patternObject) {
+    const keys = Object.keys(patternObject);
+    const outputPatternMap = new Map();
+
+    for (const key of keys) {
+      const patternId = key;
+      if (typeof patternObject[key] === "string") {
+        const patternInfo = {
+          "pattern": convertPatternToRegExp(patternObject[key]),
+          "description": null,
+          "url": null
+        };
+        outputPatternMap.set(patternId, patternInfo);
+      } else if (typeof patternObject[key] === "object" && typeof patternObject[key].pattern === "string") {
+        const value = patternObject[key];
+        const patternInfo = {
+          "pattern": convertPatternToRegExp(value.pattern),
+          "description": value.description,
+          "url": value.url
+        };
+        outputPatternMap.set(patternId, patternInfo);
+      } else {
+        throw new Error("The input pattern is not valid.");
+      }
+    }
+    
+    return outputPatternMap;
+  }
+
+  /**
+   * Translates a arry of pattern strings (no key) into a {PatternMap}.
+   * input example: ["[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]", "[DNS]-x-[DNS]-{FLIVWY}"]
+   * @param {Array} patternArray 
+   * @throws a error if input is not valid.
+   */
+  translatePatternArray(patternArray) {
+    const outputPatternMap = new Map();
+    const patternSet = new Set(patternArray);
+
+    for (const patternString of patternSet) {
+      if (typeof patternString !== "string") {
+        throw new Error ("The input pattern is not valid.");
+      }
+      const patternId = patternString;
+      const patternInfo = {
+        "pattern": convertPatternToRegExp(patternString),
+        "description": null,
+        "url": null
+      };
+      outputPatternMap.set(patternId, patternInfo);
+    }
+
+    return outputPatternMap;
+  }
 }
 
 module.exports = PredictionAssistant;
--- test/fasta/predictionAssistant/PredictionAssistant.test.js
+++ test/fasta/predictionAssistant/PredictionAssistant.test.js
@@ -14,3 +14,169 @@ test("test constructor method", ()=>{
   expect(assistant.fastaSequenceObject.size()).toBe(1);
   expect(assistant.fastaSequenceObject.getSequenceById("Unnamed sequence 1")).toBe("AAAAATTTTT");
 });
+
+/**
+ * Test translatePatternString(string) method.
+ */
+test("test translatePatternString(string) method", ()=>{
+  let assistant = new PredictionAssistant();
+  let inputPatternString = "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]";
+  let expectedPatternId = "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]";
+  let expectedPattern = "[DNS]\\w[DNS][^FLIVWY][DNESTG][DNQGHRK][^GP][LIVMC][DENQSTAGC]\\w{2}[ED]";
+  let expectedDescription = null;
+  let expectedUrl = null;
+  assistant.setPatterns(inputPatternString);
+  const patternMap = assistant.patternsWithIds;
+  expect(patternMap.keys().next().value).toEqual(expectedPatternId);
+  expect(patternMap.get(expectedPatternId).pattern).toEqual(expectedPattern);
+  expect(patternMap.get(expectedPatternId).description).toEqual(expectedDescription);
+  expect(patternMap.get(expectedPatternId).url).toEqual(expectedUrl);
+
+  assistant = new PredictionAssistant();
+  inputPatternString = " ";
+  expect(()=>{
+    assistant.setPatterns(inputPatternString);
+  }).toThrow("The input patterns cannot be blank string or an empty string.");
+
+  assistant = new PredictionAssistant();
+  inputPatternString = "";
+  expect(()=>{
+    assistant.setPatterns(inputPatternString);
+  }).toThrow("The input patterns cannot be blank string or an empty string.");
+
+  assistant = new PredictionAssistant();
+  inputPatternString = null;
+  expect(()=>{
+    assistant.setPatterns(inputPatternString);
+  }).toThrow("The input patterns cannot be null or undefined.");
+
+  assistant = new PredictionAssistant();
+  inputPatternString = undefined;
+  expect(()=>{
+    assistant.setPatterns(inputPatternString);
+  }).toThrow("The input patterns cannot be null or undefined.");
+});
+
+/**
+ * Test translatePatternArray(string) method.
+ */
+test("test translatePatternArray(string) method", ()=>{
+  let assistant = new PredictionAssistant();
+  let inputPatternArray = ["[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]", "<{AB}>"];
+  let expectedPatternId1 = "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]";
+  let expectedPatternId2 = "<{AB}>";
+  let expectedPattern1 = "[DNS]\\w[DNS][^FLIVWY][DNESTG][DNQGHRK][^GP][LIVMC][DENQSTAGC]\\w{2}[ED]";
+  let expectedPattern2 = "^[^AB]$"; 
+  let expectedDescription = null;
+  let expectedUrl = null;
+  assistant.setPatterns(inputPatternArray);
+  let patternMap = assistant.patternsWithIds;
+  expect(patternMap.size).toBe(2);
+  expect(patternMap.get(expectedPatternId1).pattern).toEqual(expectedPattern1);
+  expect(patternMap.get(expectedPatternId1).description).toEqual(expectedDescription);
+  expect(patternMap.get(expectedPatternId1).url).toEqual(expectedUrl);
+  expect(patternMap.get(expectedPatternId2).pattern).toEqual(expectedPattern2);
+  expect(patternMap.get(expectedPatternId2).description).toEqual(expectedDescription);
+  expect(patternMap.get(expectedPatternId2).url).toEqual(expectedUrl);
+
+  assistant = new PredictionAssistant();
+  inputPatternArray = ["[AB]-C" ,"" ];
+  expect(()=>{
+    assistant.setPatterns(inputPatternArray);
+  }).toThrow("The input patterns cannot be blank string or an empty string.");
+
+  assistant = new PredictionAssistant();
+  inputPatternArray = ["[AB]-C" , null ];
+  expect(()=>{
+    assistant.setPatterns(inputPatternArray);
+  }).toThrow("The input pattern is not valid.");
+
+  assistant = new PredictionAssistant();
+  inputPatternArray= ["[AB]-C" , 5 ];
+  expect(()=>{
+    assistant.setPatterns(inputPatternArray);
+  }).toThrow("The input pattern is not valid.");
+
+  assistant = new PredictionAssistant();
+  inputPatternArray = ["[AB]-C" , {"id": "A-x-x"} ];
+  expect(()=>{
+    assistant.setPatterns(inputPatternArray);
+  }).toThrow("The input pattern is not valid.");
+});
+
+/**
+ * Test translatePatternObject(string) method.
+ */
+test("test translatePatternObject(string) method", ()=>{
+  let assistant = new PredictionAssistant();
+  let inputPatternObject = {
+    "pattern1": {
+      pattern: "[DNS]-x-[DNS]-{FLIVWY}-[DNESTG]-[DNQGHRK]-{GP}-[LIVMC]-[DENQSTAGC]-x(2)-[ED]",
+      description: "This is a description",
+      url: "www.test.io"
+    }, 
+    12356: "<{AB}>"
+  };
+  let expectedPatternId1 = "pattern1";
+  let expectedPatternId2 = "12356";
+  let expectedPattern1 = "[DNS]\\w[DNS][^FLIVWY][DNESTG][DNQGHRK][^GP][LIVMC][DENQSTAGC]\\w{2}[ED]";
+  let expectedPattern2 = "^[^AB]$"; 
+  let expectedDescription = null;
+  let expectedUrl = null;
+  assistant.setPatterns(inputPatternObject);
+  let patternMap = assistant.patternsWithIds;
+  expect(patternMap.size).toBe(2);
+  expect(patternMap.get(expectedPatternId1).pattern).toEqual(expectedPattern1);
+  expect(patternMap.get(expectedPatternId1).description).toEqual("This is a description");
+  expect(patternMap.get(expectedPatternId1).url).toEqual("www.test.io");
+  expect(patternMap.get(expectedPatternId2).pattern).toEqual(expectedPattern2);
+  expect(patternMap.get(expectedPatternId2).description).toEqual(expectedDescription);
+  expect(patternMap.get(expectedPatternId2).url).toEqual(expectedUrl);
+  
+  assistant = new PredictionAssistant();
+  inputPatternObject = {
+    "pattern1": {
+      pattern: null,
+      description: "This is a description",
+      url: "www.test.io"
+    }, 
+    "123": "<{AB}>"
+  };
+  expect(()=>{
+    assistant.setPatterns(inputPatternObject);
+  }).toThrow("The input pattern is not valid.");
+
+  assistant = new PredictionAssistant();
+  inputPatternObject = {
+    "pattern1": {
+      pattern: 556,
+      description: "This is a description",
+      url: "www.test.io"
+    }, 
+    "123": "<{AB}>"
+  };
+  expect(()=>{
+    assistant.setPatterns(inputPatternObject);
+  }).toThrow("The input pattern is not valid.");
+});
+
+/**
+ * Test setPatterns(patterns) method throw error when input is invalid, null or undefined.
+ */
+test("test setPatterns(patterns) method throw error when input is invalid null or undefined.", ()=>{
+  const assistant = new PredictionAssistant();
+  const inputPatterns1 = 556;
+  const inputPatterns2 = true;
+  expect(()=>{
+    assistant.setPatterns();
+  }).toThrow("The input patterns cannot be null or undefined.");
+  expect(()=>{
+    assistant.setPatterns(null);
+  }).toThrow("The input patterns cannot be null or undefined.");
+  expect(()=>{
+    assistant.setPatterns(inputPatterns1);
+  }).toThrow("The format of input patterns is not supported.");
+  expect(()=>{
+    assistant.setPatterns(inputPatterns2);
+  }).toThrow("The format of input patterns is not supported.");
+});
